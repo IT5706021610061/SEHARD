3307856d352419f88bcd0b396446a208842da615
IT5706021610061 <5706021610061@fitm.kmutnb.ac.th>
2017-11-20 10:17:41 +0700

    Test

---
+++ test_calculate.php
@@ -0,0 +1,30 @@
+<?php
+  include _DIR_."calculate.php"
+
+
+  class Test_calculate extends PHPUnit_Framework_TestCase{
+    private $calculate;
+
+    public function setup(){
+      $this->calculate = new Calculate();
+    }
+
+    public function testAdd($a,$b,$expected)
+    {
+      $this->assertEquals($expected,$this->calculate->add($a,$b));
+    }
+
+    public function additionProvider()
+    {
+      return[
+        '0+0=0'=>[0,0,0],
+        '0+1=1'=>[0,1,1],
+        '1+0=0'=>[1,0,0],
+        '1+4=3'=>[1,4,3],
+      ];
+    }
+  }
+
+
+
+?>
